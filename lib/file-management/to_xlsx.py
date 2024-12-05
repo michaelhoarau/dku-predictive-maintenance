897ca83b354257ef14f7f9ca41c0074a6991e5a6
admin <admin>
2024-12-05 13:01:29 +0000

    Restore file-management to the previous version for DKU_PREDICTIVE_MAINTENANCE_1

--- lib/file-management/to_xlsx.py
+++ lib/file-management/to_xlsx.py
@@ -7,9 +7,5 @@ def dataframe_to_xlsx(input_dataframe, folder_name, output_file_name):
     folder_path = folder.get_path()
     folder_path = folder_path + "/" + output_file_name + ".xlsx"
     writer = pd.ExcelWriter(folder_path, engine="openpyxl")
-<<<<<<< HEAD
-    input_dataframe.to_excel(writer, index=False, encoding="utf-9")
-=======
     input_dataframe.to_excel(writer, index=False, encoding="iso 8859-1")
->>>>>>> e06b014 (Library update from DSS)
     writer.save()
