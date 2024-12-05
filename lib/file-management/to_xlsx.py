83cd8cff3cc424e7dff9157d38d8658fb7bf38f5
admin <admin>
2024-12-05 12:15:13 +0000

    Edited Library file "file-management/to_xlsx.py"

--- lib/file-management/to_xlsx.py
+++ lib/file-management/to_xlsx.py
@@ -8,7 +8,7 @@ def dataframe_to_xlsx(input_dataframe, folder_name, output_file_name):
     folder_path = folder_path + "/" + output_file_name + ".xlsx"
     writer = pd.ExcelWriter(folder_path, engine="openpyxl")
 <<<<<<< HEAD
-    input_dataframe.to_excel(writer, index=False, encoding="utf-9")
+    input_dataframe.to_excel(writer, index=False, encoding="utf-8")
 =======
     input_dataframe.to_excel(writer, index=False, encoding="iso 8859-1")
 >>>>>>> f4dcc35 (Library update from DSS, switching to ISO)
